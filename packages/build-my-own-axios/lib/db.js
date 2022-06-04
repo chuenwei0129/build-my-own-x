8a69ee375649e7388add251200bdef88a2c6a96d
chuenwei0129 <chuenwei0129@gmail.com>
2022-06-05 05:01:44 +0800

    ✨ feat: 完成 axios 的测试

--- packages/build-my-own-axios/lib/db.js
+++ packages/build-my-own-axios/lib/db.js
@@ -2,21 +2,12 @@ const Mock = require('mockjs')
 
 module.exports = () => {
   return Mock.mock({
-    'course|10': [
+    'user_info|10': [
       {
         // 属性 id 是一个自增数，起始值为 1，每次增 1
-        'id|+1': 1000,
-        course_name: '@ctitle(5,10)',
-        author: '@cname',
-        college: '@ctitle(6)',
-        'category_Id|1-6': 1
-      }
-    ],
-    'course_category|6': [
-      {
+        // json-server 必须要有 id，才能新增
         'id|+1': 1,
-        pid: -1,
-        cName: '@ctitle(4)'
+        name: '@cname()'
       }
     ]
   })
